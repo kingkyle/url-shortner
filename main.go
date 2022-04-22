a1c61ab7ef7521095870872893153daa15f8feec
kingkyle <mackings@gmail.com>
2022-04-22 19:07:46 +0100

    set up main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	app := fiber.New()
+	app.Use(logger.New())
+
+	log.Fatal(app.Listen(os.Getenv("PORT")))
 }
